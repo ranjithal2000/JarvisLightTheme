d4cb75693800f232122b6346ed187061626a510e
Abhishek Patil <Abhishek1@jktech.com>
2023-03-15 11:48:47 +0530

    New Changes

--- src/app/features/portal/portal.component.ts
+++ src/app/features/portal/portal.component.ts
@@ -5,11 +5,9 @@ import { map } from 'rxjs';
 import { SMMaterialModule } from '../../webapp-common/shared/material/material.module'
 import {ICONS} from '@common/constants';
 import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-
 export interface tags {
   name: string;
 }
-
 @Component({
   selector: 'sm-portal',
   templateUrl: './portal.component.html',
@@ -27,7 +25,6 @@ export class PortalComponent implements OnInit {
   view_url: any;
   run_url: any;
   panelOpenState: boolean;
-
   ab: any = [];
   selectedApps: any;
   Array: any = [];
@@ -45,9 +42,7 @@ export class PortalComponent implements OnInit {
   drop(event: CdkDragDrop<tags[]>) {
     moveItemInArray(this.tags, event.previousIndex, event.currentIndex);
   }
-
   constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
-
   ngOnInit(): void {
     this.fetchData();
     this.getModel();
@@ -55,7 +50,6 @@ export class PortalComponent implements OnInit {
     this.getdataset();
     this.getPipeline();
   }
-
   formdata = this.formBuilder.group({
     type: [],
     name: [],
@@ -64,7 +58,6 @@ export class PortalComponent implements OnInit {
     desc: [],
     url: []
   })
-
   formdata1 = this.formBuilder.group({
     dataset_name: ['', Validators.required],
     dataset_project: ['', Validators.required],
@@ -72,11 +65,12 @@ export class PortalComponent implements OnInit {
     version: [],
     description: []
   })
-
   formdata2 = this.formBuilder.group({
     project_name: [],
     view_url: [],
     run_url: [],
+    model_tags:[],
+    desc:[],
   })
   formdata3 = this.formBuilder.group({
     solution_name: [],
@@ -85,7 +79,6 @@ export class PortalComponent implements OnInit {
     solution_description: [],
     run_url:[]
   })
-
   formdata4 = this.formBuilder.group({
     name: [],
     version: [],
@@ -93,13 +86,15 @@ export class PortalComponent implements OnInit {
     id: [],
     url:[]
   })
-
   formdata5 = this.formBuilder.group({
     id: []
   })
   formdata6 = this.formBuilder.group({
     project_name: [],
-    id: []
+    id: [],
+    desc:[],
+    view_url:[],
+    run_url:[]
   })
   formdata7 = this.formBuilder.group({
     solution_name: [],
@@ -113,20 +108,20 @@ export class PortalComponent implements OnInit {
   formdata8 = this.formBuilder.group({
     pipeline_name:[],
     pipelineView_url:[],
-    pipeline_description:[]
+    pipeline_description:[],
+    pipeline_tags:[]
   })
   storeResponse: any;
   pipeline:any=[];
-
   // ------------------------------Modeljar section-------------------------------
   addModel() {
     debugger
     let modelName = this.formdata2.controls['project_name'].value;
     let modelViewUrl = this.formdata2.controls['view_url'].value;
     let modelRunUrl = this.formdata2.controls['run_url'].value;
-    let modelTags = 'Model';
-
-    this.http.post('http://3.108.153.122:3000/model/insertModel', { modelName, modelViewUrl, modelRunUrl, modelTags })
+    let modelTags = this.formdata2.controls['model_tags'].value;
+    let modelDescription = this.formdata2.controls['desc'].value;
+    this.http.post('http://3.108.153.122:3000/model/insertModel', { modelName, modelViewUrl, modelRunUrl, modelTags, modelDescription })
       .subscribe(response => {
         console.log(response);
         this.storeResponse = response;
@@ -135,14 +130,15 @@ export class PortalComponent implements OnInit {
       }
       )
   }
-
   editModel() {
     debugger
     let modelName = this.formdata6.controls['project_name'].value;
     let modelId = this.formdata6.controls['id'].value.toString();
     let modelTags = 'Model';
-
-    this.http.post('http://3.108.153.122:3000/model/editModel', { modelName, modelTags, modelId })
+    let modelDescription = this.formdata6.controls['desc'].value;
+    let modelRunUrl = this.formdata6.controls['run_url'].value;
+    let modelViewUrl = this.formdata6.controls['view_url'].value;
+    this.http.post('http://3.108.153.122:3000/model/editModel', { modelName, modelTags, modelId, modelDescription, modelRunUrl, modelViewUrl })
       .subscribe(response => {
         console.log("res", response);
         this.storeResponse = response;
@@ -151,7 +147,6 @@ export class PortalComponent implements OnInit {
       }
       )
   }
-
   deleteModel(data: any) {
     debugger
     console.log("modelData", data)
@@ -169,7 +164,6 @@ export class PortalComponent implements OnInit {
   dumbb: any;
   dumbb1: any;
   dumbb2: any;
-
   getModel() {
     this.http.post('http://3.108.153.122:3000/model/retrieveModels', {})
       .subscribe(response => {
@@ -187,7 +181,6 @@ export class PortalComponent implements OnInit {
     let datasetVersion = this.formdata.controls['version'].value;
     let datasetDescription = this.formdata.controls['desc'].value;
     let datasetUrl = this.formdata.controls['url'].value;
-
     this.http.post('http://3.108.153.122:3000/data/insertData', {datasetUrl, datasetName, datasetId, datasetVersion, datasetDescription })
       .subscribe(response => {
         console.log(response)
@@ -195,11 +188,8 @@ export class PortalComponent implements OnInit {
         alert(this.storeResponse.message);
         this.getdataset();
       }
-
       );
-
   }
-
   editDataset() {
     let datasetName = this.formdata4.controls['name'].value;
     let datasetVersion = this.formdata4.controls['version'].value;
@@ -215,9 +205,7 @@ export class PortalComponent implements OnInit {
         this.getdataset();
       }
       );
-
   }
-
   deleteDataset(data: any) {
     this.formdata5.controls['id'].setValue(data.datasetId);
     let datasetId = this.formdata5.controls['id'].value.toString();
@@ -231,7 +219,6 @@ export class PortalComponent implements OnInit {
       }
       );
   }
-
   getdataset() {
     this.http.post('http://3.108.153.122:3000/data/retrieveDatasets', {})
       .subscribe(response => {
@@ -241,15 +228,13 @@ export class PortalComponent implements OnInit {
       }
       )
   }
-
 // ------------------------------Pipeline Section-------------------------------
   addPipeline(){
 debugger
     let pipelineName=this.formdata8.controls['pipeline_name'].value;
     let pipelineViewUrl=this.formdata8.controls['pipelineView_url'].value;
-    let pipelineTags='';
+    let pipelineTags=this.formdata8.controls['pipeline_tags'].value;
     let pipelineDescription=this.formdata8.controls['pipeline_description'].value;
-
     this.http.post('http://3.108.153.122:3000/pipeline/insertPipeline', {pipelineName,pipelineViewUrl,pipelineTags,pipelineDescription })
       .subscribe(response => {
         console.log(response)
@@ -257,7 +242,6 @@ debugger
         alert(this.storeResponse.message);
         this.getdataset();
       }
-
       );
   }
   getPipeline(){
@@ -265,7 +249,38 @@ debugger
       .subscribe(response => {
         this.dumbb1 = response;
         this.pipeline = this.dumbb1.data;
-        console.log("abc", this.Dataset);
+        console.log("pipeline", this.pipeline);
+        
+      }
+      )
+  }
+
+  editPipeline(data:any) {
+    let pipelineName=this.formdata8.controls['pipeline_name']
+    let pipelineViewUrl=this.formdata8.controls['pipelineView_url'].value;
+    let pipelineTags=this.formdata8.controls['pipeline_tags'].value;
+    let pipelineDescription=this.formdata8.controls['pipeline_description'].value;
+    
+    this.http.post('http://3.108.153.122:3000/solution/editSolution', { pipelineName,pipelineViewUrl,pipelineTags,pipelineDescription})
+      .subscribe(response => {
+        console.log(response);
+        this.storeResponse = response;
+        alert(this.storeResponse.message)
+        this.getPipeline();
+      });
+  }
+
+  deletePipeline(data:any){
+    // console.log("solutionData", data)
+    // this.formdata7.controls['solution_id'].setValue(data.solutionId);
+    // let solutionId = this.formdata7.controls['solution_id'].value.toString();
+    let pipelineId=data.pipelineId
+    this.http.post('http://3.108.153.122:3000/pipeline/deletePipeline', { pipelineId })
+      .subscribe(response => {
+        console.log(response);
+        this.storeResponse = response;
+        alert(this.storeResponse.message)
+        this.getPipeline();
       }
       )
   }
@@ -280,15 +295,15 @@ debugger
     this.formdata4.controls['version'].setValue(data.datasetVersion);
     this.formdata4.controls['desc'].setValue(data.datasetDescription);
     this.formdata4.controls['url'].setValue(data.datasetUrl);
-
   }
-
   getDataModel(data: any) {
     console.log("clicked", data)
     this.formdata6.controls['project_name'].setValue(data.modelName);
     this.formdata6.controls['id'].setValue(data.modelId)
+    this.formdata6.controls['desc'].setValue(data.modelDescription);
+    this.formdata6.controls['run_url'].setValue(data.modelRunUrl);
+    this.formdata6.controls['view_url'].setValue(data.modelViewUrl);
   }
-
   getDataSolution(data:any){
     console.log("clickedsolution", data)
     this.formdata7.controls['solution_name'].setValue(data.solutionName)
@@ -299,8 +314,6 @@ debugger
     this.formdata7.controls['view_url'].setValue(data.solutionViewUrl)
     this.formdata7.controls['run_url'].setValue(data.solutionRunUrl)
   }
-
- 
 // ------------------------------solution jar section-------------------------------
   addSolution() {
     let solutionName = this.formdata3.controls['solution_name'].value;
@@ -317,7 +330,6 @@ debugger
         this.getSolution();
       });
   }
-
   editSolution() {
     let solutionName = this.formdata7.controls['solution_name'].value;
     let solutionId = this.formdata7.controls['solution_id'].value.toString();
@@ -326,7 +338,7 @@ debugger
     let solutionTags = this.formdata7.controls['solution_tags'].value;
     let solutionDescription = this.formdata7.controls['solution_description'].value;
     let solutionVersion = this.formdata7.controls['solution_version'].value
-   
+
     this.http.post('http://3.108.153.122:3000/solution/editSolution', {solutionId, solutionName, solutionTags, solutionDescription, solutionRunUrl, solutionVersion, solutionViewUrl })
       .subscribe(response => {
         console.log(response);
@@ -335,7 +347,6 @@ debugger
         this.getSolution();
       });
   }
-
   deleteSolution(data:any){
     console.log("solutionData", data)
     this.formdata7.controls['solution_id'].setValue(data.solutionId);
@@ -349,7 +360,6 @@ debugger
       }
       )
   }
-
   getSolution() {
     this.http.post('http://3.108.153.122:3000/solution/retrieveSolutions', {})
       .subscribe(response => {
@@ -359,11 +369,7 @@ debugger
       }
       )
   }
-
-
-
   upload() {
-
     if (this.ds == true) {
       debugger
       this.name = this.formdata.controls['type'].value;
@@ -371,72 +377,53 @@ debugger
       this.id = this.formdata.controls['id'].value;
       this.version = this.formdata.controls['version'].value;
       this.desc = this.formdata.controls['desc'].value;
-
       this.Array.push({ Type: this.name, Name: this.data_name, Id: this.id, Version: this.version, Desc: this.desc });
-
       this.http.post('https://jarvis-test-336a1-default-rtdb.firebaseio.com/Jars.json', this.Array)
         .subscribe(response => { console.log(response) }
         );
     } else if (this.mdl == true) {
-
       this.name = this.formdata2.controls['project_name'].value;
       this.id = this.formdata2.controls['view_url'].value;
       this.desc = this.formdata2.controls['run_url'].value;
-
       this.Array.push({ Type: 'Model', Name: this.name, Id: this.id, Url: this.desc });
       //Id will be view_url and Url will be run_url
       this.http.post('https://jarvis-test-336a1-default-rtdb.firebaseio.com/Jars.json', this.Array)
         .subscribe(response => { console.log(response) }
         );
     } else if (this.sln == true) {
-
       this.name = this.formdata3.controls['project_name'].value;
       this.desc = this.formdata3.controls['url'].value;
-
       this.Array.push({ Type: 'Solution', Name: this.name, Id: this.id, Url: this.desc });
-
       this.http.post('https://jarvis-test-336a1-default-rtdb.firebaseio.com/Jars.json', this.Array)
         .subscribe(response => { console.log(response) }
         );
     }else if (this.ppln == true) {
-
       this.name = this.formdata3.controls['project_name'].value;
       this.desc = this.formdata3.controls['url'].value;
-
       this.Array.push({ Type: 'Solution', Name: this.name, Id: this.id, Url: this.desc });
-
       this.http.post('https://jarvis-test-336a1-default-rtdb.firebaseio.com/Jars.json', this.Array)
         .subscribe(response => { console.log(response) }
         );
     }
-
   }
-
   editData(data: any, jar: any) {
-
     this.SelectJar(jar);
     this.formdata.controls['name'].setValue(data.datasetName);
     this.formdata.controls['version'].setValue(data.datasetVersion);
     this.formdata.controls['id'].disable();
     this.formdata.controls['desc'].setValue(data.datasetDescription);
   }
-
   reload() {
     window.location.reload();
   }
-
   isReadMore = true
-
   showText() {
      this.isReadMore = !this.isReadMore
   }
-
-
   Jars: any = [{ id: 1, name: 'Model' }, { id: 2, name: 'Dataset' }, { id: 3, name: 'Pipeline' }]
   Model: any = [{ id: 1, name: 'Local Copy' }, { id: 2, name: 'Remote Copy' }, { id: 3, name: 'Using Id' }]
   data: any = [];
   store: any = [];
-
   fetchData() {
     debugger
     this.http.get('https://jarvis-test-336a1-default-rtdb.firebaseio.com/Jars.json')
@@ -453,11 +440,9 @@ debugger
         (response) => {
           this.data = response
           console.log(this.data);
-
           this.LoadData(this.data);
         })
   }
-
   LoadData(data: any) {
     for (let i = 0; i < data.length; i++) {
       this.store = data;
@@ -465,30 +450,24 @@ debugger
         this.Engin.push(data[i]);
       } else if (this.store[i].Type == 'Model') {
         // this.Modules.push(data[i]);
-
       } else if (this.store[i].Type == 'Dataset') {
         // this.Dataset.push(data[i]);
-
       } else if (this.store[i].Type == 'Solution') {
         // this.Solution.push(data[i])
       }
     }
   }
-
   copytext() {
     navigator.clipboard.writeText(this.value1);
   }
   copytext1() {
     navigator.clipboard.writeText(this.value2);
   }
-
   ds: boolean = false;
   mdl: boolean = false;
   sln: boolean = false;
   ppln:boolean=false;
   jar: any;
-
-
   SelectJar(jar: any) {
     debugger
     this.formdata = this.formBuilder.group({
@@ -499,8 +478,6 @@ debugger
       desc: [],
       url:[]
     })
-
-
     if (jar == 'Dataset') {
       this.ds = true;
       this.mdl = false;
@@ -523,128 +500,95 @@ debugger
       this.ppln= true;
     }
   }
-
   value1: any = 'Enter all API_Data to generate the Script';
   new_dataset_name: any = '';
   dataset_id: any = '';
   new_dataset_project_name: any = '';
-
   DownloadData() {
-
     if (this.selectedIndex != 3) {
       this.selectedIndex = this.selectedIndex + 1;
     }
     console.log(this.selectedIndex);
-
     this.new_dataset_name = this.formdata1.controls['dataset_name'].value;
     this.dataset_id = this.formdata1.controls['dataset_id'].value;
     this.new_dataset_project_name = this.formdata1.controls['dataset_project'].value;
-
     this.value1 = `
 from clearml import Dataset
-
 #Get the dataset using Dataset Id
 dataset = Dataset.get("${this.dataset_id}")
-
 #Get the physical location of the dataset
 url = dataset._task.artifacts['data'].url
-  
 # Create a dataset with ClearML\`s Dataset class
 new_dataset = Dataset.create(dataset_name="${this.new_dataset_name}",
                   dataset_project="${this.new_dataset_project_name}") 
-
 #Add the example url                 
 new_dataset.add_external_files(source_url=url)
-
 # Upload dataset to ClearML server (customizable)
 new_dataset.upload() 
-
 # commit dataset changes
 new_dataset.finalize()
   `
   }
-
   setDataValues(data: any) {
     this.formdata1.controls['dataset_name'].setValue(data.Name);
     this.formdata1.controls['dataset_id'].setValue(data.Id);
   }
-
   value2: any;
   Model_Proj: any;
   model_Id: any;
-
   modelcode(data: any) {
     this.Model_Proj = data.Name;
     this.model_Id = data.Id;
-
     this.value2 =
       `from clearml import Model, Task, Logger
 import tensorflow as tf
 from tempfile import gettempdir
 import os
 import numpy as np
-
 import warnings
 warnings.filterwarnings('ignore')
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
 project_config = {
     'PROJECT_NAME' : '${this.Model_Proj}'}
-
 task = Task.init(
     project_name=project_config['PROJECT_NAME'],
     task_name='Model Inference',
     task_type='inference',
     reuse_last_task_id=False
 )
-
 logger = task.get_logger()
-
 # print(f'Loading model: af7391e2c0784dbf9e83ba3969aee923')
 model = Model('${this.model_Id}')
 print(f'\nGetting a local copy of the model : {model.id}\n') 
 model_path  = model.get_local_copy()
 print(f'model_path= {model_path}')
-
 #Load the model into keras/tf
 model = tf.keras.models.load_model(model_path)
 print(model.summary())
-
 #Load data to run inference on mnnist test data
 (x_train,y_train),(x_test,y_test) = tf.keras.datasets.mnist.load_data()
 print(x_train.shape,y_train.shape,x_test.shape,y_test.shape)
-
 #run inference
-
 sample = x_test#[:5,:,:]
 for i,img in enumerate(sample):
     pred = model.predict(np.expand_dims(img, axis=0))
     pred = np.argmax(pred, axis=1)
     logger.report_image("image", str(pred), iteration=i, image=img)`
-
   }
-
   // value1=`# create example dataset
   // from clearml import StorageManager, Dataset
-
   // # Download sample csv file
   // csv_file = StorageManager.get_local_copy(
   //     remote_url="https://vincentarelbundock.github.io/Rdatasets/csv/AER/Affairs.csv"
   // )
-
   // # Create a dataset with ClearML\`s Dataset class
   // dataset = Dataset.create(
   //     dataset_project="DatasetProject", dataset_name="HelloDataset"
   // )
-
   // # add the example csv
   // dataset.add_files(path=csv_file)
-
   // # Upload dataset to ClearML server (customizable)
   // dataset.upload()
-
   // # commit dataset changes
   // dataset.finalize()`;
-
-
 }
